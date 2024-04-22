353c0ba531d83e0a161c3d0ba45a0a663d58c6ad
Paulius Piečius <paulius_8@live.com>
2024-04-22 20:40:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This mod runs on default in-launcher Toolbox.
+
+## Version 3.0.2
+
+Create two files in `toolbox/data/definitions`
+- C_REQUEST_FINISH_GUILD_QUEST.1.def that contains one line `int32 quest`.
+- C_REQUEST_START_GUILD_QUEST.1.def that contains one line `int32 questId`.
+
+Create one file in `toolbox/data/opcodes`
+- protocol.376012.map that contains two lines `C_REQUEST_FINISH_GUILD_QUEST = 41778` and `C_REQUEST_START_GUILD_QUEST = 30633`.
---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class AutoGuildQuest {
+    constructor(mod) {
+        this.registerCommands(mod);
+
+        mod.hook('S_UPDATE_GUILD_QUEST_STATUS', 1, (event) => {
+            if (event.targets[0].completed == event.targets[0].total) {
+                setTimeout(() => { mod.send('C_REQUEST_FINISH_GUILD_QUEST', 1, { quest: event.quest }) }, 500);
+                setTimeout(() => { mod.send('C_REQUEST_START_GUILD_QUEST', 1, { questId: event.quest }) }, 500);
+            }
+        });
+    }
+
+    registerCommands(mod) {
+        mod.command.add('gq', {
+            '$none': () => {
+                mod.settings.enabled = !mod.settings.enabled;
+                mod.command.message(`${mod.settings.enabled ? 'En' : 'Dis'}abled`);
+            },
+            '$default': () => {
+                mod.command.message(`Invalid argument. Usage : gq`);
+            }
+        });
+    }
+}
+
+module.exports = { NetworkMod: AutoGuildQuest };
