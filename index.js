e3ee03fa55cabd8bd07e203c16b82af1eb27e363
Paulius Piečius <paulius_8@live.com>
2024-05-13 23:38:12 +0200

    Increase timeout to one second

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class AutoGuildQuest {
         mod.hook('S_UPDATE_GUILD_QUEST_STATUS', 1, (event) => {
             if (event.targets[0].completed == event.targets[0].total) {
                 setTimeout(() => { mod.send('C_REQUEST_FINISH_GUILD_QUEST', 1, { quest: event.quest }) }, 500);
-                setTimeout(() => { mod.send('C_REQUEST_START_GUILD_QUEST', 1, { questId: event.quest }) }, 500);
+                setTimeout(() => { mod.send('C_REQUEST_START_GUILD_QUEST', 1, { questId: event.quest }) }, 1000);
             }
         });
     }
